d39dbb5a66362da9d430535a8f326891a9aab30a
codin-AWMKAR <omkaroneofakind@gmail.com>
2023-12-25 02:54:47 +0530

    todo4

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,19 +1,54 @@
 const router = require("express").Router();
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-//SIGN IN
+//SIGN UP
 
 router.post("/register",async (req,res) =>
 {
     try {
         const {email,username,password} = req.body;
-        const user = new User({email,username,password});
+        const hashpassword = bcrypt.hashSync(password);
+        const user = new User({email,username,password:hashpassword});
         await user.save().then(()=>res.status(200).json({user:user}))
     } 
     catch (error) {
+        
         res.status(400).json({
             message:"User Already Exists"
         })
     }
-})
+});
+
+//SIGN IN
+router.post("/signin", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Please Sign Up First"
+            });
+        }
+
+        const isPasswordCorrect = bcrypt.compare(req.body.password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({
+                message: "Please Enter Correct Password"
+            });
+        }
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({
+            others
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Error occurred during sign-in"
+        });
+    }
+});
+
+
 module.exports = router;
