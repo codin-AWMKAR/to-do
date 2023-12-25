b1bcaab11560fded7e87d31626c21af5a67b662c
codin-AWMKAR <omkaroneofakind@gmail.com>
2023-12-25 22:53:49 +0530

    todo5

---
+++ Backend/routes/list.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const List = require ("../models/list");
+
+router.post("/addTask", async(req,res)=>{
+   try {
+    const {title,body,email} = req.body;
+    const  existingUser = await User.findOne({ email });
+    if(existingUser){
+        const list = new List({title,body,user: existingUser});
+        await list.save().then(()=>{
+            res.status(200).json({list})
+        });
+        existingUser.list.push(list);
+        existingUser.save();
+    }
+   } catch (error) {
+     console.log(error);
+   }
+})
+module.exports = router;
